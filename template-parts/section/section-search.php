1707ffae99952551bc1d4f4381e6a7655f05ea00
milton.higaki <milton.higaki@gmail.com>
2022-01-26 20:50:10 -0300

    search

--- template-parts/section/section-search.php
+++ template-parts/section/section-search.php
@@ -1,8 +1,114 @@
 <div class="search-section-area ssa"> 
   <div class="search-area-inner"> 
     <div class="search-contents"> 
-      <form method="GET" action="<?php echo esc_url( home_url( '/' ) ); ?>" id="search-adv">
-        <?php get_search_form( true ); ?>
+      <form method="GET" action="<?php echo esc_url( home_url( '/' ) ); ?>" id="search-adv"> 
+        <div class="row"> 
+          <div class="col-lg-3 col-md-6 col-sm-6 col-6"> 
+            <?php $terms = get_terms( array(
+                'taxonomy' => 'properties_city',
+                'orderby' => 'name',
+                'order' => 'ASC',
+                'hide_empty' => true
+            ) ) ?>
+            <div class="form-group"> 
+              <?php if( !empty( $terms ) ) : ?>
+                <select class="selectpicker search-fields" name="area"> 
+                  <?php foreach( $terms as $term ) : ?>
+                    <option>
+                      <?php echo $term->name; ?>
+                    </option>
+                  <?php endforeach; ?>                                                                             
+                </select>
+              <?php endif; ?> 
+            </div>             
+          </div>           
+          <div class="col-lg-3 col-md-6 col-sm-6 col-6"> 
+            <?php $terms = get_terms( array(
+                'taxonomy' => 'properties_uf',
+                'orderby' => 'name',
+                'order' => 'ASC',
+                'hide_empty' => true
+            ) ) ?>
+            <div class="form-group"> 
+              <?php if( !empty( $terms ) ) : ?>
+                <select class="selectpicker search-fields" name="property-status"> 
+                  <?php foreach( $terms as $term ) : ?>
+                    <option>
+                      <?php echo $term->name; ?>
+                    </option>
+                  <?php endforeach; ?>                                       
+                </select>
+              <?php endif; ?> 
+            </div>             
+          </div>           
+          <div class="col-lg-3 col-md-6 col-sm-6 col-6"> 
+            <?php $terms = get_terms( array(
+                'taxonomy' => 'properties_status',
+                'orderby' => 'name',
+                'order' => 'ASC',
+                'hide_empty' => true
+            ) ) ?>
+            <div class="form-group"> 
+              <?php if( !empty( $terms ) ) : ?>
+                <select class="selectpicker search-fields" name="property-types"> 
+                  <?php foreach( $terms as $term ) : ?>
+                    <option>
+                      <?php echo $term->name; ?>
+                    </option>
+                  <?php endforeach; ?>                                                                             
+                </select>
+              <?php endif; ?> 
+            </div>             
+          </div>           
+          <div class="col-lg-3 col-md-6 col-sm-6 col-6">
+            <?php $terms = get_terms( array(
+                'taxonomy' => 'properties_types',
+                'orderby' => 'name',
+                'order' => 'ASC',
+                'hide_empty' => true
+            ) ) ?>
+            <div class="form-group"> 
+              <?php if( !empty( $terms ) ) : ?>
+                <select class="selectpicker search-fields" name="location"> 
+                  <?php foreach( $terms as $term ) : ?>
+                    <option>
+                      <?php echo $term->name; ?>
+                    </option>
+                  <?php endforeach; ?>                                                                             
+                </select>
+              <?php endif; ?> 
+            </div>             
+          </div>           
+        </div>         
+        <div class="row"> 
+          <div class="col-lg-3 col-md-6 col-sm-6 col-6"> 
+            <div class="form-group"> 
+              <select class="selectpicker search-fields" name="max-area"> 
+                <option>
+                  <?php echo get_field( 'field_5fac3f659f6a1' ); ?>
+                </option>                                                                                                      
+              </select>               
+            </div>             
+          </div>           
+          <div class="col-lg-3 col-md-6 col-sm-6 col-6"> 
+            <div class="form-group"> 
+              <select class="selectpicker search-fields" name="min-area"> 
+                <option>
+                  <?php echo get_field( 'field_5faf728e8aa5e' ); ?>
+                </option>                                                                                                      
+              </select>               
+            </div>             
+          </div>           
+          <div class="col-lg-3 col-md-6 col-sm-6 col-6">              
+</div>           
+          <div class="col-lg-3 col-md-6 col-sm-6 col-6"> 
+            <div class="form-group"> 
+              <button class="search-button">
+                <?php _e( 'Procurar', 'pam' ); ?>
+              </button>               
+            </div>             
+          </div>           
+        </div>         
       </form>       
     </div>     
   </div>   
